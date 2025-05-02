cb4fd5b628112c631bdcebefa7ede75c267c961c
Stefan Binder <stefan.binder89@gmail.com>
2025-05-02 20:55:38 +0200

    fix(ui-accounts): accounts button prefix

--- packages/ui-accounts/src/lib/accounts-button.tsx
+++ packages/ui-accounts/src/lib/accounts-button.tsx
@@ -4,7 +4,7 @@ import { Button } from '@nx-release-playground/ui-shared';
 export function AccountsButton({children}: {children: React.ReactNode}) {
   return (
     <div className={styles['container']}>
-      <Button>{children} ACCOUNTS</Button>
+      <Button>{children} <i>ACCOUNTS</i></Button>
     </div>
   );
 }
