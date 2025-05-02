13e876b1d0e14623d72e4a312051e134274b9468
Stefan Binder <stefan.binder89@gmail.com>
2025-05-02 20:52:47 +0200

    fix(ui-shared): button spec and style

--- packages/ui-shared/src/lib/button.spec.tsx
+++ packages/ui-shared/src/lib/button.spec.tsx
@@ -4,7 +4,7 @@ import Button from './button';
 
 describe('Button', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Button />);
+    const { baseElement } = render(<Button>test</Button>);
     expect(baseElement).toBeTruthy();
   });
 });
