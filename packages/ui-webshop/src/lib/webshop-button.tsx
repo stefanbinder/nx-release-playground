e9fd96026813cdc64c07d9f7627805cd71347990
Stefan Binder <stefan.binder89@gmail.com>
2025-05-02 21:19:32 +0200

    fix(ui-webshop): style prefix

--- packages/ui-webshop/src/lib/webshop-button.tsx
+++ packages/ui-webshop/src/lib/webshop-button.tsx
@@ -4,7 +4,7 @@ import styles from './webshop-button.module.scss';
 export function WebshopButton({children}: {children: React.ReactNode}) {
   return (
     <div className={styles['container']}>
-      <Button>{children} <span>WEBSHOP</span></Button>
+      <Button>{children} <span className={styles['prefix']}>WEBSHOP</span></Button>
     </div>
   );
 }
