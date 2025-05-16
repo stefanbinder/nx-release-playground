48dd362d8128db936fcbcd8d856f19b094257b06
Stefan Binder <stefan.binder89@gmail.com>
2025-05-16 10:48:06 +0200

    fix: studio improvment

--- packages/studio/src/lib/studio.tsx
+++ packages/studio/src/lib/studio.tsx
@@ -3,7 +3,7 @@ import styles from './studio.module.scss';
 export function Studio() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Studio</h1>
+      <h1>Welcome to Studio 1</h1>
     </div>
   );
 }
